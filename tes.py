af75d842b35937724f524a8d9452d37ae589ea5c
Kruceo <rafa2015krueger@gmail.com>
2022-11-12 14:31:20 -0300

    repo fix

--- tes.py
+++ tes.py
@@ -1,22 +1,25 @@
 import numpy as np
-
+import time 
 import cv2 as cv
-
+import threading
 input_rtsp = "rtsp://externo:0QbRoF7Xyuka@186.208.217.215:64554/cam/realmonitor?channel=1&subtype=1"
 input_rtspA = "rtsp://externo:0QbRoF7Xyuka@186.208.217.215:64554/cam/realmonitor?channel=1&subtype=1"
 
-vcap = cv.VideoCapture(input_rtsp)
-vcapa = cv.VideoCapture(input_rtspA)
+vcap = cv.VideoCapture(input_rtsp,cv.CAP_FFMPEG)
+vcapa = cv.VideoCapture(input_rtspA,cv.CAP_ANY)
 #vcapb = cv.VideoCapture(input_rtspA)
 #vcapc = cv.VideoCapture(input_rtsp)
 vcap.set(cv.CAP_PROP_BUFFERSIZE,3)
-vcap.set(cv.CAP_PROP_FPS,1/60)
+
+
+print(vcap.get(cv.CAP_PROP_FPS))
 
 
 
 screenWidth = 720
 screenHeight = 480
-img = np.zeros((screenHeight,screenWidth,3),np.uint8)
+img = np.zeros((screenHeight,screenWidth,2),np.uint8)
+
 cv.imshow('VIDEO',img)
 cv.setWindowProperty('VIDEO',cv.WND_PROP_FULLSCREEN,cv.WINDOW_FULLSCREEN)
 
@@ -37,8 +40,8 @@ while(1):
     img[0:screenHeight//2,screenWidth//2:screenWidth] = smalla
     img[screenHeight//2:screenHeight,0:screenWidth//2] = smalla
     img[screenHeight//2:screenHeight,screenWidth//2:screenWidth] = small
-  
     cv.imshow('VIDEO', img)
+    time.sleep(1/60)
     if cv.waitKey(1) == ord('q'):
         break
 vcap.release()
